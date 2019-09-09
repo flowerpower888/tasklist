b68ae729da108434f375183d6809b0ab5d3c1ee8
hruck <oingooboingo@gmail.com>
2019-09-09 16:07:23 +0300

    eslint check

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var add = document.querySelector('.btn-add'),
-  list = document.querySelector('.list'),
+const list = document.querySelector('.list'),
   input = document.querySelector('input');
 
 if (localStorage.tasks) {
@@ -7,13 +6,13 @@ if (localStorage.tasks) {
 }
 input.focus();
 
-//add sailor moon
+//  add sailor moon
 
 document.querySelector('h1').addEventListener('click', function() {
   document.querySelector('img').classList.toggle('visible');
 });
 
-//create task
+//  create task
 
 document.querySelector('form').addEventListener('submit', function(e) {
   e.preventDefault();
@@ -22,80 +21,18 @@ document.querySelector('form').addEventListener('submit', function(e) {
   storeTasks();
 });
 
-//cross out and remove
+//  cross out and remove
 
 list.addEventListener('click', function(e) {
-    if (e.target.classList.contains('close')) {
-      e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-    } else if (e.target.classList.contains('list-item')) {
-      e.target.classList.toggle('crossout');
-    };
-    storeTasks();
-  });
+  if (e.target.classList.contains('close')) {
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+  } else if (e.target.classList.contains('list-item')) {
+    e.target.classList.toggle('crossout');
+  };
+  storeTasks();
+});
 
 
 function storeTasks() {
   window.localStorage.tasks = list.innerHTML;
 }
-
-
-
-
-
-// var todoList = [];
-//
-//
-// //create new array with values of 'in'
-// document.getElementById('add').onclick = function () {
-//
-//   var task = document.getElementById('in').value;
-//   todoList.push(task);
-//   document.getElementById('in').value = null;
-//
-//
-// //add li
-//   var li = document.createElement('li');
-//   li.innerHTML = task;
-//   document.getElementById('outlist').appendChild(li);
-//
-//
-// //create close button
-//   function createCloseButton() {
-//       var span = document.createElement("span");
-//       span.innerHTML = 'x';
-//       span.className = "close";
-//       li.appendChild(span);
-//
-//       // for (i = 0; i <= li.length; i++){
-//       //   createCloseButton(li[i]);
-//       // }
-//
-// //remove task
-//       span.onclick = function() {
-//           document.getElementById('outlist').removeChild(li);
-//       }
-//   }
-//   createCloseButton();
-//
-// //cross out task
-//   li.onclick = function () {
-//     li.style.textDecoration="line-through";
-//   }
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// // function out () {
-// //   var out = '';
-// //   todoList.forEach (function(item) {
-// //     out += item + '<br>';
-// //   })
-// //   document.getElementById('outlist').innerHTML = out;
-// // }
-// // out();
